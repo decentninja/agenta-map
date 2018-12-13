37fc57a23d6efda23cb9d9f73e16c8349fd88c8f
Linn <andreas.linn@dice.se>
2018-12-13 17:15:02 +0100

    Stuff

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+extern crate stdweb;
+
+
+fn main() {
+    let canvas = stdweb::web::document().create_element("canvas").unwrap();
+    stdweb::web::document.body().unwrap().
+    println!("HI");
+}
